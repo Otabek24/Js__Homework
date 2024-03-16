09d5e3b4eec99fc802c672e92acc6533646d0525
Otabek <Otabek>
2024-03-16 14:57:40 +0500

    sdgf

---
+++ script.js
@@ -0,0 +1,29 @@
+let name = prompt('Ismingizni kiriting')
+
+console.log('Sizning ismingiz ' + name)
+
+let age = prompt('Yoshingizni kiriting')
+
+console.log('Sizning yoshingiz ' + age + 'da')
+
+let question = prompt('6+3=?')
+
+console.log('Sizning javobingiz ' + question + ". To'g'ri javob:6+3=9")
+
+let question2 = prompt('10-3=?')
+
+console.log('Sizning javobingiz ' + question2 + ". To'g'ri javob:10-3=7")
+
+let question3 = prompt('12/3=?')
+
+console.log('Sizning javobingiz ' + question3 + ". To'g'ri javob:12/3=4")
+
+let question4 = prompt('9*9=?')
+
+console.log('Sizning javobingiz ' + question4 + ". To'g'ri javob:9*9=81")
+
+let question5 = prompt('13%2=?')
+
+console.log('Sizning javobingiz ' + question5 + ". To'g'ri javob:13%2=1")
+
+alert('Konsolni ochib tekshirishingiz mumkin')
